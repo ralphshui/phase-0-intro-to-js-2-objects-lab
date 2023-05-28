7bcb6cbca33f895e9f3251b9e15c210667695422
Ralph Shui <rskc.2112@gmail.com>
2023-05-28 03:40:28 -0400

    writing object functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name : "ralph", 
+    streetAddress :"123 broadway street", 
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+   
+
+function deleteFromEmployeeByKey(employee, key){
+    const newRoster = {...employee};
+    delete newRoster[key];
+
+    return newRoster;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
+}
